72d972f04870b2123b8c1dc491f1f0379e24a51b
bed-deb <julus06@o2.pl>
2025-08-14 19:53:47 +0200

    Update login1.js

--- login1.js
+++ login1.js
@@ -17,7 +17,7 @@ function sprawdzDane() {
     var haslo = document.getElementById("haslo").value;
 
     var users = {
-        "Qx_2!n$Vr@k#Tm": "Zp_5@b$Hq!n#Fx",
+        "Vn_7!q$Tp@w#Lm": "Qz_8@x$Kr!c#Vp",
     };
 
     // Weryfikacja danych
